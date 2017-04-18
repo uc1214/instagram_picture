06687ff2394f22375a072684f0c440a817d5abbb
Yushi Saijo <yushi.saijo@gmail.com>
2017-04-18 23:06:05 +0900

    revise layout of image

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,7 +7,7 @@ module ApplicationHelper
     else
       img_url = 'no_image.png'
     end
-    image_tag(img_url, alt: user.name)
+    image_tag(img_url, alt: user.name, :class => "img")
   end
 end
 
